5d953fc6d5197664e2f721b2d448528e294143d4
Fetusmagnet <221936G@mymail.nyp.edu.sg>
2024-02-05 04:20:51 +0800

    Add project files.

---
+++ EmailSender.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace AppSec_Assignment
+{
+    public class EmailSender 
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mail = "testemail@gmail.com";
+            var password = "ABcd12345678";
+
+            var client = new SmtpClient()
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                Credentials = new NetworkCredential(mail, password)
+
+            };
+
+            return client.SendMailAsync(from: mail, recipients: email, subject, message);
+        }
+    }
+}
